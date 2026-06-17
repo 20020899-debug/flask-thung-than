70682e6e7f924d85e6b89e3afbecc961ad715a23
20020899-debug <20020899@vnu.edu.vn>
2026-06-17 08:53:07 +0700

    Update san_thao_tac.py

--- flaskapp1/routes/san_thao_tac.py
+++ flaskapp1/routes/san_thao_tac.py
@@ -24,14 +24,13 @@ def san_thao_tac():
         B2 = get_float("chieu_rong")
         C2 = get_float("chieu_cao")
         D2 = get_float("do_day_ton")
-        E2 = get_float("so_chan")
 
         F11 = get_float("thanh_ngang")
         G11 = get_float("thanh_doc")
 
         khoi_luong_san = (
             ((A2*(B2/500) + B2*2) * 160 * D2 * THEP / MM3)
-            + (C2 * 300 * D2 * E2 * THEP / MM3)
+            + (C2 * 282 * D2 * 4 * THEP / MM3)
             + (A2 * B2 * 11 / 1_000_000)
             + ((A2 + B2 - 240) * 2 * 140 * 3 * THEP / MM3)
             + 29.3
