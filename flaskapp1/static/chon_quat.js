802f2bd8d52cdae6799f897eb0e1a1541ac3bfce
20020899-debug <20020899@vnu.edu.vn>
2026-06-20 13:23:35 +0700

    Update chon_quat.js

---
+++ flaskapp1/static/chon_quat.js
@@ -0,0 +1,101 @@
+async function timQuat() {
+
+    const D =
+        parseFloat(
+            document.getElementById("diameter").value
+        );
+
+    const V =
+        parseFloat(
+            document.getElementById("velocity").value
+        );
+
+    const P =
+        parseFloat(
+            document.getElementById("pressure").value
+        );
+
+    if (!D || !V || !P) {
+        alert("Nhập đủ dữ liệu");
+        return;
+    }
+
+    // mm -> m
+    const d = D / 1000;
+
+    // m3/h
+    const Q =
+        Math.PI *
+        d *
+        d /
+        4 *
+        V *
+        3600;
+
+    const res =
+        await fetch(
+            `/tim_quat?q=${Q}&p=${P}`
+        );
+
+    const fans =
+        await res.json();
+
+    let html = `
+        <h3>
+        Lưu lượng tính toán:
+        ${Math.round(Q).toLocaleString()}
+        m³/h
+        </h3>
+    `;
+
+    if (fans.length === 0) {
+
+        html += `
+            <p>
+            Không tìm thấy quạt phù hợp
+            </p>
+        `;
+
+    } else {
+
+        fans.forEach(fan => {
+
+            html += `
+                <div class="fan-card">
+
+                    <h3>${fan.Model}</h3>
+
+                    <p>
+                    Loại:
+                    ${fan.Loai}
+                    </p>
+
+                    <p>
+                    Lưu lượng:
+                    ${fan.Qmin.toLocaleString()}
+                    -
+                    ${fan.Qmax.toLocaleString()}
+                    m³/h
+                    </p>
+
+                    <p>
+                    Áp suất:
+                    ${fan.Pmin.toLocaleString()}
+                    -
+                    ${fan.Pmax.toLocaleString()}
+                    Pa
+                    </p>
+
+                    <p>
+                    Công suất:
+                    ${fan.Kw} kW
+                    </p>
+
+                </div>
+            `;
+        });
+    }
+
+    document.getElementById("result").innerHTML =
+        html;
+}
